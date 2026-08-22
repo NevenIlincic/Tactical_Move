d2be8dba517d210e05e2642c24c5e4a2401f4ddf
Neven Ilincic <nevenilincic@gmail.com>
2026-08-22 15:36:09 +0200

    [FIX] Light effect when shooting not disappearing

--- Scenes/Effects/gun_blast_effect.gd
+++ Scenes/Effects/gun_blast_effect.gd
@@ -6,18 +6,14 @@ func do_effect(position: Vector2):
 	global_position = position
 	emitting = true
 	point_light_2d.enabled = true
-	
-	# Kreiramo Tween za treperenje
+
 	var tween = create_tween()
-	
-	# Primer: svetlost menja energiju sa 1 na 0.2 nekoliko puta u toku trajanja čestica
-	# Pretpostavka je da efekat traje npr. 0.2 ili 0.3 sekunde (prilagodi po potrebi)
+
 	tween.tween_property(point_light_2d, "energy", 1.5, 0.05)
 	tween.tween_property(point_light_2d, "energy", 1.0, 0.05)
 	tween.tween_property(point_light_2d, "energy", 1.2, 0.05)
 	
-	# Kada se efekat završi, gasi svetlo i vrati energiju na normalu
-	finished.connect(func(): 
+	tween.tween_callback(func():
 		point_light_2d.enabled = false
-		point_light_2d.energy = 1.0 # vratimo na podrazumevanu vrednost za sledeći put
-	, CONNECT_ONE_SHOT)
+		point_light_2d.energy = 1.0
+	)
--- Scripts/Managers/audio_manager.gd
+++ Scripts/Managers/audio_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const M_4A_1_RIFLE_SOUND = preload("uid://c3ncljfia8w0c")
+const BUTTON_SOUND = preload("uid://c6ouqh8rlq0bk")
 
 var current_level: Level
 
@@ -10,6 +11,14 @@ func set_current_level(level: Level):
 func play_gun_shoot_sound():
 	var audio: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
 	audio.stream = M_4A_1_RIFLE_SOUND
-	current_level.add_child(audio)
+	#current_level.add_child(audio)
+	get_tree().root.add_child(audio)
+	audio.play()
+	audio.finished.connect(func(): audio.queue_free())
+
+func play_upgrade_sound():
+	var audio: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
+	audio.stream = BUTTON_SOUND
+	get_tree().root.add_child(audio)
 	audio.play()
 	audio.finished.connect(func(): audio.queue_free())
