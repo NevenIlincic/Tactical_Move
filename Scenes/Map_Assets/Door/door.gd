9d95aa2f88f9c86367b6fce3f5750f6cb3cb129a
Neven Ilincic <nevenilincic@gmail.com>
2026-08-22 14:43:35 +0200

    [ADD] Light effect when shooting

--- Scenes/Map_Assets/Door/door.gd
+++ Scenes/Map_Assets/Door/door.gd
@@ -1,17 +1,20 @@
 class_name Door extends Control
 
 var is_opened: bool = false
+@onready var door_slam_sound: AudioStreamPlayer2D = $Door_Slam_Sound
 
 func top_indicator_activated():
 	if not is_opened:
 		is_opened = true
 		rotation_degrees = 90
+		door_slam_sound.play()
 
 	#print(area.is_in_group("soldier"))
 func bottom_indicator_activated():
 	if not is_opened:
 		is_opened = true
 		rotation_degrees = -90
+		door_slam_sound.play()
 
 
 func _on_top_indicator_area_entered(area: Area2D) -> void:
